be6c1eed420f684f6bbb4da3598e54ac077e2ec5
jean-h <student-00-88d1d4d72a09@qwiklabs.net>
2026-07-02 14:21:49 +0000

    Update Docker

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 	})
 
 	http.HandleFunc("/blue", blueHandler)
-	/*http.HandleFunc("/red", redHandler)*/
+	http.HandleFunc("/red", redHandler)
 	log.Println("Starting server on :8080...")
 	http.ListenAndServe(":8080", nil)
 }
@@ -47,9 +47,9 @@ func blueHandler(w http.ResponseWriter, r *http.Request) {
 	png.Encode(w, img)
 }
 
-// func redHandler(w http.ResponseWriter, r *http.Request) {
-// 	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
-// 	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
-// 	w.Header().Set("Content-Type", "image/png")
-// 	png.Encode(w, img)
-// }
+func redHandler(w http.ResponseWriter, r *http.Request) {
+	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
+	w.Header().Set("Content-Type", "image/png")
+	png.Encode(w, img)
+}
