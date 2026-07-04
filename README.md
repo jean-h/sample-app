bcaa94da92364b8be5e08c88930eb100f69e85cb
jean-h <student-00-88d1d4d72a09@qwiklabs.net>
2026-07-04 17:33:21 +0000

    Dev work

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 Some dev work
-Some more
--- main.go
+++ main.go
@@ -34,7 +34,6 @@ func main() {
 	})
 
 	http.HandleFunc("/blue", blueHandler)
-	http.HandleFunc("/red", redHandler)
 	log.Println("Starting server on :8080...")
 	http.ListenAndServe(":8080", nil)
 }
